fc42112d8efafe2c8c697c622225d0fad4c2e993
Liam Kopp <liam.r.kopp@gmail.com>
2015-12-01 12:10:41 -0500

    add everything
    everything created from before git/github

---
+++ scripts.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+
+    
+    //Animate main page images
+    $('.center-block').mouseover(function(){
+        $('p', this).animate({opacity:1});
+        $('img', this).animate({opacity:0});
+        
+    }).mouseout(function(){
+        $('p', this).animate({opacity:0});
+        $('img', this).animate({opacity:1});
+    })
+        
+    
+    
+    
+    // animate.css function
+    // http://www.telegraphicsinc.com/2013/07/how-to-use-animate-css/
+    function animationHover(element, animation) {
+        element = $(element);
+        element.hover(
+            function () {
+                element.addClass('animated ' + animation);        
+            },
+            function () {
+                //wait for animation to finish before removing classes
+                window.setTimeout(function () {
+                    
+                    element.removeClass('animated ' + animation);
+                    element.addClass('animated fadeIn');
+                    
+                }, 200);         
+            });
+    }
+
+});
